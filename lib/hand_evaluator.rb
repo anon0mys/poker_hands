169daf25fe30ecdd88d8dff402ee129dc24cb397
Evan Wheeler <anon0mys87@gmail.com>
2018-08-30 15:24:08 -0600

    Update initialize method to split hand string into array. Add collect by number method

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -2,7 +2,14 @@ class HandEvaluator
   attr_reader :hand_one, :hand_two
 
   def initialize(hand_one, hand_two)
-    @hand_one = hand_one
-    @hand_two = hand_two
+    @hand_one = hand_one.split(' ')
+    @hand_two = hand_two.split(' ')
+  end
+
+  def collect_by_number(hand)
+    hand.reduce(Hash.new(0)) do |collection, card|
+      collection[card[0]] += 1
+      collection
+    end
   end
 end
--- test/hand_evaluator_test.rb
+++ test/hand_evaluator_test.rb
@@ -1,5 +1,6 @@
-require 'minitest/autorun'
+require '''minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 require './lib/hand_evaluator'
 
 class HandEvaluatorTest < MiniTest::Test
@@ -10,7 +11,19 @@ class HandEvaluatorTest < MiniTest::Test
     evaluator = HandEvaluator.new(hand_one, hand_two)
 
     assert_instance_of HandEvaluator, evaluator
-    assert_equal evaluator.hand_one, hand_one
-    assert_equal evaluator.hand_two, hand_two
+    assert_equal evaluator.hand_one, hand_one.split(' ')
+    assert_equal evaluator.hand_two, hand_two.split(' ')
+  end
+
+  def test_it_can_collect_cards_by_number
+    hand_one = '5H 5C 6S 7S KD'
+    hand_one_numbers = {'5' => 2, '6' => 1, '7' => 1, 'K' => 1}
+    hand_two = '2C 3S 8S 8D TD'
+    hand_two_numbers = {'2' => 1, '3' => 1, '8' => 2, 'T' => 1}
+
+    evaluator = HandEvaluator.new(hand_one, hand_two)
+
+    assert_equal hand_one_numbers, evaluator.collect_by_number(evaluator.hand_one)
+    assert_equal hand_two_numbers, evaluator.collect_by_number(evaluator.hand_two)
   end
 end
